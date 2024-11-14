b3cf5b0235879605c31e9870577468868315ebac
nrb007 <nrb121903@gmail.com>
2024-11-14 13:49:29 -0500

    asdra

--- A3/Single-Linked-List.cpp
+++ A3/Single-Linked-List.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
-
+//Struct that contains one data item and a next pointer
 struct slinklist{
+    //Stores the data for a given node
     int data;
+    //Points to the address of the next node in the list
     struct slinklist* next;
 };
 
+//Allows other functions to use and manipulate the struct
 typedef struct slinklist node;
+//Forces the start varible to be NULL meaning the list is empty by default
 node* start = NULL;
 
 
@@ -21,15 +25,23 @@ int countnode(node* start);
 
 int main(){
 
+    //Size of list
     int number;
     cout << "Number of Nodes: ";
     cin >> number;
     
+    //Creates list then displays it
     createlist(number);
     transverse();
+    
+    //Deletes element then displays it 
     detele_at_mid();
     transverse();
+    
+    //Adds new element from getnode to the existing list instead of making a new one
     node* existingNode = getnode();
+    
+    //Inserts the new element then displays it
     insert_in_mid(existingNode);
     transverse();
     
@@ -37,46 +49,71 @@ int main(){
     return 0;
 }
 
-
 node* getnode(){
+    //A newnode is create and added to memory
     node* newnode;
     newnode = (node*)malloc(sizeof(node));
+    
+    //Ask user for data inside of list
     cout << "\nEnter Data: ";
     cin >> newnode -> data;
+    
+    //Assigns the newest data to NULL meaning it's the end of the list 
     newnode -> next = NULL;
+    
+    //Then returns the pointer to the new node so the next node can point to the previous
     return newnode;
 }
 
+
 void createlist(int n){
+    
     int i;
     node* newnode;
     node* temp;
+    
+    //Loops for the length of the list 
     for(i = 0; i < n; i++){
+        //Creates new node (with a pointer to the previous)
         newnode = getnode();
+        //If list is empty fill it 
         if(start == NULL){
             start = newnode;
         }
+        //If the list isnt empty 
         else{
+            //Sets temp to the start of the list
             temp = start;
-            while(temp-> next != NULL){
+            //Goes through the list until is reaches the last node
+            while(temp -> next != NULL){
                 temp = temp -> next;
             }
+            //This then links the last node to the new last node
             temp -> next = newnode;
         }
     }
 }
 
+
 void detele_at_mid(){
     int ctr = 1, pos, nodectr;
     node *temp, *prev;
+    
+    //Checks if list is empty
     if(start==NULL){
         cout << "\nEmpty List";
         return;
     }
+    //If not it actually executes
     else{
+        //Sets position for deletion
         cout << "\nEnter postion to delete: ";
         cin >> pos;
+        
+        //A counter
         nodectr = countnode(start);
+        
+        //
         if (pos > nodectr){
             cout << "\nNode doesn't exist";
         }
