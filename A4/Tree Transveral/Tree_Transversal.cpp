ee1d1a7956f06d7d6af855a26a013305b9652c38
nrb007 <nrb121903@gmail.com>
2024-11-28 11:36:23 -0500

    sdf

--- A4/Tree Transveral/Tree_Transversal.cpp
+++ A4/Tree Transveral/Tree_Transversal.cpp
@@ -2,98 +2,58 @@
 Nathan Boyden 
 COP 3530
 Assignment 4
-11/26/24
+11/28/24
 */
 
-#include <stdio.h>
 #include <iostream>
-
-//Defines max size of stack and where the top is (the bottom by default)
-#define MAX 6
-int stack[MAX];
-int top = 0;
-
 using namespace std;
 
-//Prototypes
-int menu();
-void display();
-void pop();
-void push();
+// Binary Tree Node
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
 
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
 
-int main(){
-    int ch;
-    do{
-        ch = menu();
-        switch(ch){
-            case 1:
-                push();
-                break;
-            case 2:
-                pop();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                exit(0);
-        }
-        
+// Insert into Binary Tree
+void insert(TreeNode*& root, int value) {
+    if (root == nullptr) {
+        root = new TreeNode(value); // Create new node if root is null
+        return;
     }
-    while(1);
+    if (value < root->val) {
+        insert(root->left, value); // Insert into left subtree
+    } else {
+        insert(root->right, value); // Insert into right subtree
+    }
+}
+
+// In-order Traversal (recursive)
+void inorderTraversal(TreeNode* root) {
+    if (root == nullptr) return;
+    inorderTraversal(root->left);
+    cout << root->val << " ";
+    inorderTraversal(root->right);
+}
+
+// Main Function
+int main() {
+    TreeNode* root = nullptr;
+
+    // Insert values into the binary tree
+    insert(root, 12);
+    insert(root, 7);
+    insert(root, 3);
+    insert(root, 3);
+    insert(root, 9);
+    insert(root, 14);
+    insert(root, 8);
+
+    // Perform in-order traversal
+    cout << "In-order Traversal: ";
+    inorderTraversal(root);
+
     return 0;
-}
-
-//Display a menu and returns that menu value to the main where that process can occur
-int menu(){
-    int ch;
-    cout << "\n 1. Push\n 2. Pop\n 3. Display\n 4. Quit\n ";
-    cin >> ch;
-    return ch;
-}
-
-//Displays whats currently in the stack and displays nothing if empty
-void display(){
-    int i;
-    if(top == 0){
-        cout << "\n Stack is empty";
-        return;
-    }
-    else{
-        cout << "\n Element/s in Stack: ";
-        for(i = 0; i < top; i++){
-            cout << stack[i] << " ";
-        }
-    }
-}
-
-//Checks if empty if not removed the top and moves the top to the next element 
-void pop(){
-    if(top == 0){
-        cout << "\n ... ";
-        return;
-    }
-    else{
-        cout << "\n ..." << stack[--top];
-        
-    }
-
-}
-
-//Checks if empty if not takes user data and adds to stack then makes new data the top
-void push(){
-    int data;
-    if(top == MAX){
-        cout << "\n Stack Overflow";
-        return;
-    }
-    else{
-        cout << "\n Enter Data: ";
-        cin >> data;
-        stack[top] = data;
-        ++top;
-        cout << "\n Data pushed into the stack";
-    }
-    
 }
